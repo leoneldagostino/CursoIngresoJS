51912656409b2690d7c2c16cc88b96cd6db9f80a
LeonelDagostino28 <74281280+LeonelDagostino28@users.noreply.github.com>
2021-07-08 00:00:10 -0300

    trabajo practico

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,51 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let mostrarSuma;
+
+    precioUno=parseInt(document.getElementById("txtIdPrecioUno").value);
+    precioDos=parseInt(document.getElementById("txtIdPrecioDos").value);
+    precioTres=parseInt(document.getElementById("txtIdPrecioTres").value);
+
+    //mostrar suma
+    mostrarSuma= precioUno+precioDos+precioTres;
+    alert(`la suma de los precio da: ${mostrarSuma} `);
 }
 function Promedio () 
 {
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let mostrarPromedio;
+
+    precioUno=parseFloat(document.getElementById("txtIdPrecioUno").value);
+    precioDos=parseFloat(document.getElementById("txtIdPrecioDos").value);
+    precioTres=parseFloat(document.getElementById("txtIdPrecioTres").value);
 	
+    //mostrar promedio
+    mostrarPromedio=(precioUno+precioDos+precioTres)/3;
+    alert(`el promedio de los precios es ${mostrarPromedio}`);
 }
 function PrecioFinal () 
 {
-	
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let sumaDePrecios;
+    let iva;
+    let precioMasIva;
+
+    precioUno=parseFloat(document.getElementById("txtIdPrecioUno").value);
+    precioDos=parseFloat(document.getElementById("txtIdPrecioDos").value);
+    precioTres=parseFloat(document.getElementById("txtIdPrecioTres").value);
+    sumaDePrecios=precioUno+precioDos+precioTres;
+    iva=(precioUno+precioDos+precioTres)*21/100;
+
+    //mostrar precio final
+    precioMasIva=sumaDePrecios+iva;
+    alert(`El precio final de los productos es ${precioMasIva}`);
+
 }
