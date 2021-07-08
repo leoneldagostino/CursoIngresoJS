5da73b2f69a8d0ed4c05ced25b9bb57f29f2072e
LeonelDagostino28 <74281280+LeonelDagostino28@users.noreply.github.com>
2021-07-07 23:29:05 -0300

    realizacion tarea E/S nro 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	//declaracion de variables
+	let importe;
+	let descuento;
+	let importeConDescuento;
+
+	//toma de datos
+	importe=parseInt(document.getElementById("txtIdImporte").value);
+
+	//operaciones
+	descuento=importe*0.25;
+
+	//motrardescuento 
+	importeConDescuento=importe-descuento;
+	document.getElementById("txtIdResultado").value=importeConDescuento;
+
 }
