a882e829f6ac2b4ac764872d8150a4d6c9d3adc9
Leonel <leeonel_@live.com>
2021-07-13 13:00:22 -0300

    clase-3-ejercicios-1-2

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,9 @@
 function mostrar()
-{
+{	
 	let mes;
 
 	mes=document.getElementById("txtIdMes").value;
-
+/*
 	if(mes == "Enero"){
 		alert("que comiences bien el año");
 	}
@@ -16,7 +16,32 @@ function mostrar()
 	else if(mes == "Diciembre"){
 		alert("Felices Fiestas!!");
 	}
+	*/
+
+	/*switch(variable)
+	case(casos) valor:
+		...
+		break;
+	case valor:
+		...
+		break;->me saca del switch y continua 
+	*/
+	switch(mes){
+		case "Enero":
+			alert("Que empieces bien el año");
+			break;
+		case "Marzo":
+			alert("A clases");
+			break;
+		case "Julio":
+			alert("Se viene las vacaciones!!");
+			break;
+		case "Diciembre":
+			alert("Felices Fiestas!!");
+			break;
+		}
+}	
+	
 
 
-
-}//FIN DE LA FUNCIÓN
+//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,56 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes;
 
+	mes=document.getElementById("txtIdMes").value;
+/*
+	switch(mes){
+		case "Enero":
+			alert("aun falta para invierno");
+			break;
+		case "Febrero":
+			alert("aun falta para invierno");
+			break;
+		case "Marzo":
+			alert("aun falta para invierno");
+			break;
+		case "Abril":
+			alert("aun falta para invierno");
+			break;
+		case "Mayo":
+			alert("aun falta para invierno");
+			break;
+		case "Junio":
+			alert("aun falta para invierno");
+			break;
+		case "Julio":
+			alert("Abrigate que hace frio");
+			break;
+		case "Agosto":
+			alert("Abrigate que hace frio");
+			break;
+		case "Septiembre":
+			alert("Ya pasamos el frio, ahora hace calor");
+			break;
+		case "Octubre":
+			alert("Ya pasamos el frio, ahora hace calor");
+			break;
+		case "Noviembre":
+			alert("Ya pasamos el frio, ahora hace calor");
+			break;
+		case "Diciembre":
+			alert("Ya pasamos el frio, ahora hace calor");
+			break;
+		}
+*/
 
-
-
+	if(mes =="Enero" || mes=="Febrero" || mes=="Marzo" || mes=="Abril" || mes=="Mayo" || mes=="Junio"){
+		alert("Falta para el invierno");
+	}
+	else if(mes == "Julio" || mes == "Agosto"){
+		alert("Estamos en invierno,Abrigate");
+	}
+	else {
+		alert("ya pasamos el frio,ahora hace calor");
+	}
 }//FIN DE LA FUNCIÓN
