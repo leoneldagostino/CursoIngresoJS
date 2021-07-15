5a31405838617bd3ec1969476a820e0fc9d26854
Leonel <leeonel_@live.com>
2021-07-15 16:36:58 -0300

    clase 15/7-hacer tp 4

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,58 @@
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	let numero,
+	contador,
+	acumulador;
 
-	contador=0;
-	acumulador=0;
+
+	contador=0
+
+	while(contador < 5){
+		console.log("el contador ahora vale"+ contador);
+		contador++;
+		numero=parseInt(prompt("ingrese el primer numero:"));
+		acumulador+=numero;//o acumulador=acumulador+numero; 
+
+
+	}
+	
+	let promedio=acumulador/contador;
+
+	document.getElementById("txtIdSuma").value=acumulador;
+	document.getElementById("txtIdPromedio").value=promedio;
 	
 	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
-}//FIN DE LA FUNCIÓN
+	
+	
+	
+	
+	/*
+	let suma,
+	promedio,
+	numero,
+	numero1,numero2,numero3,numero4,numero5;
+
+	numero=0;
+
+	while(// condicion ) {
+		numero1=parseInt(prompt("Ingrese un numero:"));
+		
+
+	}
+	
+	numero2=parseInt(prompt("Ingrese un numero:"));
+	numero3=parseInt(prompt("Ingrese un numero:"));
+	numero4=parseInt(prompt("Ingrese un numero:"));
+	numero5=parseInt(prompt("Ingrese un numero:"));
+
+
+	suma=numero1+numero2+numero3+numero4+numero5;
+	promedio=suma/5;
+	
+	document.getElementById("txtIdSuma").value=suma;
+	document.getElementById("txtIdPromedio").value=promedio;
+	*/
+
+	
+}
+//FIN DE LA FUNCIÓN
