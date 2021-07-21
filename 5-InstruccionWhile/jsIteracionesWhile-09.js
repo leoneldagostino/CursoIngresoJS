e48e6b438c7dab5ef309fb8274cc6ad2e5e297e4
Rocio Fernandez <rociofernandez@MacBook-Air-de-Rocio.local>
2021-07-21 20:45:45 -0300

    tarea 21-7

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,7 +1,7 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
-function mostrar()
+function mostrar()/*
 {	// declarar variables
 	var banderaDelPrimero;
 	var numeroIngresado;
@@ -17,5 +17,23 @@ function mostrar()
 		respuesta=prompt("desea continuar?");
 	}
 	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
+	txtIdMinimmo.value=numeroMinimo;*/
+{
+
+	let numero,
+	numMax,
+	numMin,
+	respuesta;
+	flag=1
+
+	do{
+		numero=parseInt(prompt("Ingrese el numero que quiere ingresar: "));
+
+
+		respuesta=prompt("Quiere ingresar otro numero?");
+	}while(respuesta=='s');
+
+
+
+
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -12,18 +12,44 @@ hasta que el usuario quiera, mostrar:
 9-Diferencia entre positivos y negativos, (positvos-negativos). */
 function mostrar()
 {
-	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	let contado=0;
+	let numero;
+	let numerosPositivos=0;
+	let numeroNegativos=1;
+	let sumaNegativos;
+	let sumaPositivos;
+	let cantidadPositivos;
+	let cantidadNegativos;
+	let flag=1
+	let numerosPares;
+	let cantidadCeros;
+	let promediosPositivos;
+	let promediosNegativos;
+	let respuesta;
+	let diferenciaPositivosNegativos
+	
+	do{
 
-	respuesta="si";
+		numero=parseInt(prompt("Ingrese un numero: "));
+		if(numero > 0){
+			numerosPositivos+=numero;
+			sumaPositivos=numerosPositivos;
+		}
+		else{
+			numeroNegativos-=numero;
+			sumaNegativos=numeroNegativos;
+			flag=0;
+		}
+		if(flag){
+			numeroNegativos=0;
+		}
 
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+		respuesta=prompt("Usted quiere ingresar otro numero? ");
+	}while(respuesta=='s');
+
+	document.write("Suma de nros positivos: "+sumaPositivos+", Suma de los nros negativos: " +sumaNegativos);
+
+
+
 }//FIN DE LA FUNCIÓN
